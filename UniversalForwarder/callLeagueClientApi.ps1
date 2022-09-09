6712cf132dee7abe49115a0073205e9493480893
ekraemer-splunk <68660333+ekraemer-splunk@users.noreply.github.com>
2022-09-09 12:19:18 -0500

    Added UF files

---
+++ UniversalForwarder/callLeagueClientApi.ps1
@@ -0,0 +1,13 @@
+add-type @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+    public bool CheckValidationResult(
+        ServicePoint srvPoint, X509Certificate certificate,
+        WebRequest request, int certificateProblem) {
+            return true;
+        }
+ }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+(Invoke-WebRequest -URI https://127.0.0.1:2999/liveclientdata/allgamedata -UseBasicParsing).Content
